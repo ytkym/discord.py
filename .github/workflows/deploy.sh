cf2d395d6f33e1d003d8b7a6140af8981d642815
ytkym <66817517+ytkym@users.noreply.github.com>
2020-09-18 09:20:09 +0900

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # outputディレクトリ内のファイルをrsync over SSHで転送
-rsync -acvz --delete ./ ${{ secrets.SERVER_HOST }}@${{ secrets.SERVER_USERNAME }}:/${{ secrets.DEST_DIR }}
+rsync -acvz --delete ./ ${{ secrets.SERVER_USERNAME }}@${{ secrets.SERVER_HOST}}:/${{ secrets.DEST_DIR }}
 # サービスを再起動
-ssh user@remote "python3 discord.py/examples/basic_bot.py"
+ssh ${{ secrets.SERVER_USERNAME }}@${{ secrets.SERVER_HOST}} "python3 discord.py/examples/basic_bot.py"
