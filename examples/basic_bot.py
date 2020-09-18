a34c6fff9d217bcba9d820253437dd97f0fed0ff
ytkym <66817517+ytkym@users.noreply.github.com>
2020-09-18 17:50:27 +0900

    Update basic_bot.py

--- examples/basic_bot.py
+++ examples/basic_bot.py
@@ -31,10 +31,10 @@ async def add(ctx, left: int, right: int):
     await ctx.send(left + right)
 
 
-@bot.command()
-async def sub(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left - right)
+# @bot.command()
+# async def sub(ctx, left: int, right: int):
+#     """Adds two numbers together."""
+#     await ctx.send(left - right)
 
 @bot.command()
 async def roll(ctx, dice: str):
